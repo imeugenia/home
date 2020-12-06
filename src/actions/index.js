98e9494f8dbaee15f86128731463e6097e6edcb0
jenya <jevgenija.zigisova@raisin.com>
2020-12-06 10:09:57 +0100

    Remove temperature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,103 +1,101 @@
-import axios from 'axios';
-const contentful = require('contentful')
+const contentful = require("contentful");
 
-export const FETCH_POSTS = 'FETCH_POSTS'
-export const FETCH_PROJECTS = 'FETCH_PROJECTS'
-export const FETCH_CODE = 'FETCH_CODE'
-export const FETCH_PICS = 'FETCH_PICS'
-export const FETCH_TEMP = 'FETCH_TEMP'
-export const FETCH_CONTACTS = 'FETCH_CONTACTS'
-export const FETCH_LOCATION = 'FETCH_LOCATION'
+export const FETCH_POSTS = "FETCH_POSTS";
+export const FETCH_PROJECTS = "FETCH_PROJECTS";
+export const FETCH_CODE = "FETCH_CODE";
+export const FETCH_PICS = "FETCH_PICS";
+export const FETCH_TEMP = "FETCH_TEMP";
+export const FETCH_CONTACTS = "FETCH_CONTACTS";
+export const FETCH_LOCATION = "FETCH_LOCATION";
 
-const API_SPACE_ID = 'cczllbmg75ay'
-const API_KEY = 'c1782ea2b12077bbc6f35d39d322af845aff60a0486717208c9803b520c28d9b'
+const API_SPACE_ID = "cczllbmg75ay";
+const API_KEY =
+  "c1782ea2b12077bbc6f35d39d322af845aff60a0486717208c9803b520c28d9b";
 
 const client = contentful.createClient({
   space: API_SPACE_ID,
-  accessToken: API_KEY
-})
+  accessToken: API_KEY,
+});
 
 export function fetchPosts() {
-  const request = client.getEntries({
-    'content_type': 'card'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "card",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_POSTS,
-    payload: request
-  }
+    payload: request,
+  };
 }
 
 export function fetchProjects() {
-  const request = client.getEntries({
-    'content_type': 'projects'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "projects",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_PROJECTS,
-    payload: request
-  }
+    payload: request,
+  };
 }
 
 export function fetchCode() {
-  const request = client.getEntries({
-    'content_type': 'code'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "code",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_CODE,
-    payload: request
-  }
+    payload: request,
+  };
 }
 
 export function fetchPics() {
-  const request = client.getEntries({
-    'content_type': 'pics'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "pics",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_PICS,
-    payload: request
-  }
+    payload: request,
+  };
 }
 export function fetchContacts() {
-  const request = client.getEntries({
-    'content_type': 'contacts'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "contacts",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_CONTACTS,
-    payload: request
-  }
-}
-
-export function fetchTemperature() {
-  const request = axios.get(`https://api.apixu.com/v1/current.json?key=49caf41d8bbb48da8ff175933171910&q=Daugavpils`);
-  return {
-    type: FETCH_TEMP,
-    payload: request
-  }
+    payload: request,
+  };
 }
 
 export function fetchLocation() {
-  const request = client.getEntries({
-    'content_type': 'location'
-  })
-  .then(entries => {
-    return entries  
-  })
+  const request = client
+    .getEntries({
+      content_type: "location",
+    })
+    .then((entries) => {
+      return entries;
+    });
   return {
     type: FETCH_LOCATION,
-    payload: request
-  }
+    payload: request,
+  };
 }
