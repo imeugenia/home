0ce2fc60a232f0992f088f579bb872ceb6e3b6c3
jenya <jevgenija.zigisova@raisin.com>
2020-12-23 11:59:48 +0100

    Change page title

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
+import Symbol from "../media/happy-ghost.svg";
 import "../index.css";
 
 const HEADERDATA = {
-  textPart1: "I am Eugenia.",
-  textPart2: "A web engineer,",
-  textPart3: "UI design enthusiast.",
+  textPart1: "Eugenia Zigisova",
+  textPart2: "Web developer",
+  textPart3: "Speaker",
 };
 class Header extends Component {
   render() {
     return (
       <header className="flexbox nowrap">
-        <div className="col-1" />
-        <div className="col-2">
-          <h2>
-            {HEADERDATA.textPart1}
-            <br />
-            <span className="red-text">{HEADERDATA.textPart2}</span>
-            <br />
-            {HEADERDATA.textPart3}
-          </h2>
-        </div>
+        <h1>
+          {HEADERDATA.textPart1}
+          <br />
+          <span className="red-text">{HEADERDATA.textPart2}</span>
+          <br />
+          {HEADERDATA.textPart3}
+        </h1>
       </header>
     );
   }
