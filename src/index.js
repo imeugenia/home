a460e5d42a0ad4c48ca4d98fabff294db810cd46
jenya <jevgenija.zigisova@raisin.com>
2021-03-13 08:39:06 +0100

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import promise from 'redux-promise'
-import { createStore, applyMiddleware } from 'redux'
-import reducers from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import promise from "redux-promise";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
-
-registerServiceWorker()
+  document.getElementById("root")
+);
